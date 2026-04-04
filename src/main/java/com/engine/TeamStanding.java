aceeab2f82215fbd02f70da0f3633821d689eb8b
erengndd <erengundogdu35@hotmail.com>
2026-04-04 19:30:46 +0300

    Engine and Football classes

---
+++ src/main/java/com/engine/TeamStanding.java
@@ -0,0 +1,51 @@
+package com.engine;
+
+import com.interfaces.ITeam;
+
+public class TeamStanding {
+
+    private ITeam team;
+    private int played;
+    private int won;
+    private int drawn;
+    private int lost;
+    private int scored;
+    private int conceded;
+    private int points;
+
+    public TeamStanding(ITeam team) {
+        this.team = team;
+        this.played = 0;
+        this.won = 0;
+        this.drawn = 0;
+        this.lost = 0;
+        this.scored = 0;
+        this.conceded = 0;
+        this.points = 0;
+    }
+
+    public void update(int goalsFor, int goalsAgainst, int pointsEarned) {
+        this.played++;
+        this.scored += goalsFor;
+        this.conceded += goalsAgainst;
+        this.points += pointsEarned;
+
+        if (pointsEarned == 3) won++;
+        else if (pointsEarned == 1) drawn++;
+        else lost++;
+    }
+
+    public ITeam getTeam() { return team; }
+    public int getPlayed() { return played; }
+    public int getWon() { return won; }
+    public int getDrawn() { return drawn; }
+    public int getLost() { return lost; }
+    public int getScored() { return scored; }
+    public int getConceded() { return conceded; }
+    public int getPoints() { return points; }
+    public int getGoalDiff() { return scored - conceded; }
+    public double getAverage() {
+        if (conceded == 0) return scored;
+        return (double) scored / conceded;
+    }
+}
--- src/main/java/com/football/FootballStandingsRules.java
+++ src/main/java/com/football/FootballStandingsRules.java
@@ -1,39 +1,56 @@
 package com.football;
 
+import com.engine.TeamStanding;
 import com.interfaces.IStandingsRules;
+
 import java.util.Comparator;
+import java.util.Random;
 
 public class FootballStandingsRules implements IStandingsRules {
-    private static final int POINTS_WIN  = 3;
-    private static final int POINTS_DRAW = 1;
-    private static final int POINTS_LOSS = 0;
-    @Override
-    public int getPointsForWin() {
-        return 0;
-    }
 
     @Override
-    public int getPointsForDraw() {
-        return 0;
-    }
+    public int getPointsForWin() { return 3; }
 
     @Override
-    public int getPointsForLoss() {
-        return 0;
-    }
+    public int getPointsForDraw() { return 1; }
 
     @Override
-    public boolean hasOvertime() {
-        return false;
-    }
+    public int getPointsForLoss() { return 0; }
+
+    @Override
+    public boolean hasOvertime() { return false; }
 
     @Override
     public Comparator<Object> getComparator() {
-        return null;
+        return (a, b) -> {
+            TeamStanding tsA = (TeamStanding) a;
+            TeamStanding tsB = (TeamStanding) b;
+
+            if (tsB.getPoints() != tsA.getPoints()) {
+                return tsB.getPoints() - tsA.getPoints();
+            }
+
+            double avgDiff = tsB.getAverage() - tsA.getAverage();
+            if (avgDiff != 0) {
+                return avgDiff > 0 ? 1 : -1;
+            }
+
+            // 3) Goals scored
+            if (tsB.getScored() != tsA.getScored()) {
+                return tsB.getScored() - tsA.getScored();
+            }
+
+            if (tsB.getGoalDiff() != tsA.getGoalDiff()) {
+                return tsB.getGoalDiff() - tsA.getGoalDiff();
+            }
+
+            return new Random().nextInt(3) - 1;
+        };
     }
 
     @Override
     public String describeTiebreakers() {
-        return "";
+        return "1) Points  2) Goal Average (For/Against)  " +
+                "3) Goals Scored  4) Goal Difference  5) Coin Toss";
     }
 }
