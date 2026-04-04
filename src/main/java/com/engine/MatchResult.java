2d107a2be73d8ee274eb8344f6423f188c4b659e
erengndd <erengundogdu35@hotmail.com>
2026-04-04 22:14:00 +0300

    MatchResult ekledim

---
+++ src/main/java/com/engine/MatchResult.java
@@ -0,0 +1,41 @@
+package com.engine;
+import com.interfaces.ITeam;
+
+import java.util.List;
+
+public class MatchResult {
+
+    private MatchScore score;
+    private ITeam winner;
+    private ITeam homeTeam;
+    private ITeam awayTeam;
+    private List<String> injuredPlayers;
+    private int pointsHome;
+    private int pointsAway;
+
+    public MatchResult(MatchScore score, ITeam winner, ITeam homeTeam, ITeam awayTeam,
+                       List<String> injuredPlayers, int pointsHome, int pointsAway) {
+        this.score = score;
+        this.winner = winner;
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.injuredPlayers = injuredPlayers;
+        this.pointsHome = pointsHome;
+        this.pointsAway = pointsAway;
+    }
+
+    public MatchScore getScore() { return score; }
+    public ITeam getWinner() { return winner; }
+    public ITeam getHomeTeam() { return homeTeam; }
+    public ITeam getAwayTeam() { return awayTeam; }
+    public List<String> getInjuredPlayers() { return injuredPlayers; }
+    public int getPointsHome() { return pointsHome; }
+    public int getPointsAway() { return pointsAway; }
+    public boolean isDraw() { return winner == null; }
+
+    @Override
+    public String toString() {
+        return homeTeam.getName() + " " + score + " " + awayTeam.getName()
+                + " | Winner: " + (isDraw() ? "Draw" : winner.getName());
+    }
+}
