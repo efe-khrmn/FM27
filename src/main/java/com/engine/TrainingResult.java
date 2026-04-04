f940ca0cf3341c5cf43213156878e30e10bd3a8b
R. Efe Kahraman <efekhrmn333ksk@gmail.com>
2026-04-04 22:17:55 +0300

    added TRainingResult

---
+++ src/main/java/com/engine/TrainingResult.java
@@ -0,0 +1,40 @@
+package com.engine;
+
+import com.interfaces.IPlayer;
+
+public class TrainingResult {
+
+    private IPlayer player;
+    private int attackXPGained;
+    private int defenseXPGained;
+    private int staminaDelta;
+    private int compatDelta;
+    private boolean overallChanged;
+
+    public TrainingResult(IPlayer player, int attackXPGained, int defenseXPGained,
+                          int staminaDelta, int compatDelta, boolean overallChanged) {
+        this.player = player;
+        this.attackXPGained = attackXPGained;
+        this.defenseXPGained = defenseXPGained;
+        this.staminaDelta = staminaDelta;
+        this.compatDelta = compatDelta;
+        this.overallChanged = overallChanged;
+    }
+
+    public IPlayer getPlayer() { return player; }
+    public int getAttackXPGained() { return attackXPGained; }
+    public int getDefenseXPGained() { return defenseXPGained; }
+    public int getStaminaDelta() { return staminaDelta; }
+    public int getCompatDelta() { return compatDelta; }
+    public boolean isOverallChanged() { return overallChanged; }
+
+    @Override
+    public String toString() {
+        return player.getName()
+                + " | AttackXP +" + attackXPGained
+                + " | DefenseXP +" + defenseXPGained
+                + " | Stamina " + staminaDelta
+                + " | Compat +" + compatDelta
+                + " | Overall changed: " + overallChanged;
+    }
+}
